a04793a5e32c1d053a4e3c66c528ea319b36ff72
vhpraneeth <praneeth.18bce7147@vitap.ac.in>
2021-04-28 15:02:26 +0530

    Edited files

--- file_encryptor.php
+++ file_encryptor.php
@@ -2,6 +2,82 @@
 
 /* To use in other file - include 'file_encryptor.php'; */
 
+
+// referred https://riptutorial.com/php/example/25499/symmetric-encryption-and-decryption-of-large-files-with-openssl
+define('FILE_ENCRYPTION_BLOCKS', 10000);
+
+function encryptFile($filename, $key)   {
+    $source = $filename;
+    $dest = $filename . ".enc";
+    $key = substr(sha1($key, true), 0, 16);
+    $iv = openssl_random_pseudo_bytes(16);
+
+    $error = false;
+    if ($fpOut = fopen($dest, 'w')) {
+        // Put the initialzation vector to the beginning of the file
+        fwrite($fpOut, $iv);
+        if ($fpIn = fopen($source, 'rb')) {
+            while (!feof($fpIn)) {
+                $plaintext = fread($fpIn, 16 * FILE_ENCRYPTION_BLOCKS);
+                $ciphertext = openssl_encrypt($plaintext, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $iv);
+                // Use the first 16 bytes of the ciphertext as the next initialization vector
+                $iv = substr($ciphertext, 0, 16);
+                fwrite($fpOut, $ciphertext);
+            }
+            fclose($fpIn);
+        } else {
+            $error = true;
+        }
+        fclose($fpOut);
+    } else {
+        $error = true;
+    }
+
+    // delete file - pending
+
+    return $error ? false : $dest;
+}
+
+function decryptFile($filename, $key)   {
+    $source = $filename . ".enc";
+    $dest = $filename;
+    $key = substr(sha1($key, true), 0, 16);
+
+    $error = false;
+    if ($fpOut = fopen($dest, 'w')) {
+        if ($fpIn = fopen($source, 'rb')) {
+            // Get the initialzation vector from the beginning of the file
+            $iv = fread($fpIn, 16);
+            while (!feof($fpIn)) {
+                $ciphertext = fread($fpIn, 16 * (FILE_ENCRYPTION_BLOCKS + 1)); // we have to read one block more for decrypting than for encrypting
+                $plaintext = openssl_decrypt($ciphertext, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $iv);
+                // Use the first 16 bytes of the ciphertext as the next initialization vector
+                $iv = substr($ciphertext, 0, 16);
+                fwrite($fpOut, $plaintext);
+            }
+            fclose($fpIn);
+        } else {
+            $error = true;
+        }
+        fclose($fpOut);
+    } else {
+        $error = true;
+    }
+
+    return $error ? false : $dest;
+}
+
+
+$key = "770A8A65DA156D24EE2A093277530142";
+
+//encryptFile("testing_file.pdf", $key);
+decryptFile("testing_file.pdf", $key);
+
+//decryptFile("log.txt", $key);
+
+
+
+/* old code - not working
 function encrypt_file($filename, $key) 	{
 	
     // get file as string
@@ -45,13 +121,6 @@ function decrypt_file($filename, $key) 	{
     echo $plaintext;
     // write to file
     file_put_contents($filename, $plaintext);
-}
-
-$key = "770A8A65DA156D24EE2A093277530142";
-
-//encrypt_file("testing_file.pdf", $key);
-//decrypt_file("testing_file.pdf", $key);
-
-decrypt_file("log.txt", $key);
+}*/
 
 ?>
