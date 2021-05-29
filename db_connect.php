a7c5377ddd99436dfa4986a0251d63f411373333
VH Praneeth <vhpraneeth2001@gmail.com>
2021-05-29 18:47:45 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,15 +1,34 @@
 <?php
+$servername = "sql209.ultimatefreehost.in";  // "sql209.ultimatefreehost.in";
+$username = "ltm_28498913";
+$password = "i32f5GN4sSLw93pq13";
+$dbname = "ltm_28498913_project_db";
 
-// Connecting to the Database
+/* Online hosting
 $servername = "mysql-29585-0.cloudclusters.net:29585";
 $username = "root";
 $password = "testtest";
 $database="project_db";
+*/
 
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn)
+    die("Failed: ". mysqli_connect_error());
+    // "Sorry we failed to connect: "
 
-if (!$conn){
-    die("Sorry we failed to connect: ". mysqli_connect_error());
+
+/*$sql = "SELECT * FROM test_table";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo $row['Value'] . "<br>";
+  }
+} else {
+  echo "0 results";
 }
+
+mysqli_close($conn);*/
 
 ?>
